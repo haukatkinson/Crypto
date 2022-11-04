dd1f554199a182f4edd9acd9766237a4f1326573
haukatkinson <haukatkinson@aol.com>
2022-11-04 18:59:40 +0000

    added index

--- src/api/routes.py
+++ src/api/routes.py
@@ -20,7 +20,7 @@ def handle_hello():
 @api.route('/crypto', methods=['GET'])
 def forward_resp():
     resp = requests.get(
-        'hhttps://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+        'hhttps://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false'
     ).json()
     
     return jsonify(crypto = resp)
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -51,7 +51,7 @@ const getState = ({ getStore, getActions, setStore }) => {
       },
       getCrypto: () => {
         fetch(
-          `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false`
+          `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false`
         )
           .then((resp) => resp.json())
           .then((data) => {
